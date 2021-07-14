9a3da1f44501a3b9be68db522cabf2f2d8821055
KalnaBarzashka <kalina.barzashka@gmail.com>
2021-07-14 23:52:16 +0300

    initial commit

---
+++ TheMatrixAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheMatrixAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
