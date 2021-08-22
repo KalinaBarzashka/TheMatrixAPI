dfa639e1f91b6cc21df8fa6ff9de035f3ef768b5
KalnaBarzashka <kalina.barzashka@gmail.com>
2021-08-22 21:46:41 +0300

    Updated Login and Register

--- src/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,8 +44,8 @@ namespace TheMatrixAPI.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -75,14 +75,12 @@ namespace TheMatrixAPI.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         
             if (ModelState.IsValid)
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
